9030eb5660049a92bcc03357eeba20c4b174c471
benitazz <benitazz@gmail.com>
2019-05-15 07:01:31 +0200

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { CustomValidationModule } from './custom-validations/custom-validation.module';
+import { ControlsModule } from './controls/controls.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustomValidationModule,
+    ControlsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
